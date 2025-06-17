672f2621cfa6d8039b1b8321b7a93d377acd2e9b
rmakiyama <rmakiyama.io@gmail.com>
2025-06-18 03:46:34 +0900

    fix brew path

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,6 @@ abbr configf 'code ~/.config/fish/config.fish'
 abbr sourcef 'source ~/.config/fish/config.fish'
 abbr configs 'code ~/.config/starship.toml'
 
-command -q brew && brew shellenv | source
+command -q /opt/homebrew/bin/brew && /opt/homebrew/bin/brew shellenv | source
 command -q starship && starship init fish | source
 command -q ~/.local/bin/mise && ~/.local/bin/mise activate fish | source
